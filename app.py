466582ac4c0072724fb48da7cf43664f7f18d1e0
nmutlu98 <nmutlu17@ku.edu.tr>
2021-02-20 18:16:15 +0300

    Added index

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask import jsonify
+from flask import render_template
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route('/')
 def home():
-    return jsonify({'name':'Jimit',
-                    'address':'India'})
+    return render_template("index.html")
 @app.route('/home')
 def contact():
     return jsonify({'name':'Necla',
